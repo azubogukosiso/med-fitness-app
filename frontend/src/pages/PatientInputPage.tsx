ce773c402a7d2b3caaffd5c86239517754da897b
Azubogu Kosiso <azuboguko@gmail.com>
2026-01-20 14:10:47 +0100

    modify: Clean up PatientInputPage by removing unused code
    Removed unused imports and logout functionality.

--- frontend/src/pages/PatientInputPage.tsx
+++ frontend/src/pages/PatientInputPage.tsx
@@ -1,21 +1,8 @@
-// LIBRARY IMPORTS
-import { useNavigate } from "react-router-dom";
-
 // FUNCTION IMPORTS
 import PatientInputFormComponent from "../components/PatientInputFormComponent";
 import PageHeaderComponent from "../components/PageHeaderComponent";
 
-import { useAuthContext } from "./../hooks/useAuthContext";
-
 const PatientInputPage = () => {
-  const { logout } = useAuthContext();
-  const navigate = useNavigate();
-
-  const handleLogout = async () => {
-    await logout();
-    navigate("/login");
-  };
-
   return (
     <>
       <PageHeaderComponent
