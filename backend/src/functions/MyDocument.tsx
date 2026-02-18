9c98344ede2058af8dd0f3e3a543098362933bce
Azubogu Kosiso <azuboguko@gmail.com>
2026-02-18 09:22:18 +0100

    modify: Adjust certificate padding to contain all content in one page

--- backend/src/functions/MyDocument.tsx
+++ backend/src/functions/MyDocument.tsx
@@ -76,7 +76,7 @@ const styles = StyleSheet.create({
     fontFamily: "Times-Roman",
   },
   footer: {
-    marginTop: 57,
+    marginTop: 50,
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
